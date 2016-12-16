2d5e83076616f7cdf2794449321ebf2b20226bcb
minghui.fan <minghui.fan@generalbiologic.com>
2016-12-16 15:12:09 +0800

    spring

--- src/test/java/com/fmh/solr/SolrPush.java
+++ src/test/java/com/fmh/solr/SolrPush.java
@@ -3,11 +3,11 @@ package com.fmh.solr;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class SolrPush {
 
@@ -15,10 +15,14 @@ public class SolrPush {
 	public void test() throws IOException, SolrServerException {
 		SolrClient client = new HttpSolrClient.Builder("http://localhost:8080/solr/solr/").build();
 		SolrInputDocument doc = new SolrInputDocument();
-		doc.setField("id",5);
-		doc.setField("title"," solr的智能提示");
-		doc.setField("count",30);
-		client.add(doc);
+		client.deleteByQuery("*:*");
+		int i = 1;
+		for (String str : Arrays.asList("北大青岛","北大核心","北大法宝","北大荒","北大招生网","北大光华管理学院")) {
+			doc.setField("id", i++);
+			doc.setField("title", str);
+			doc.setField("count", 12);
+			client.add(doc);
+		}
 		client.commit();
 	}
 }
